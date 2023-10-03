f5db3ac5319291f25da21cc462db76205eeff489
ajalapraise <akintomiisin@gmail.com>
2023-10-03 13:44:49 +0100

    update

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -15,6 +15,10 @@ const Navbar = () => {
     setIsNavOpen(!isNavOpen);
   };
 
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   const isLinkActive = (linkPath: string) => {
     return router.pathname === linkPath;
   };
@@ -23,7 +27,7 @@ const Navbar = () => {
     <div className="bg-purple3 border-b-[1px] border-[#FFFFFF2E] w-full font-extrabold text-[white] p-1">
       <div className={`flex   px-10  ${isNavOpen ? " min-h-screen justify-start items-start mt-8" : "min-h-[60px] md:min-h-[90px] items-center justify-between"} md:px-20 relative  z-[9999]`}>
         <div className="w-2/5 flex justify-left items-center `">
-          <Link href="/" className={`flex items-center ${revalia.className}`}>
+          <Link href="/" className={`flex items-center ${revalia.className}`} onClick={closeNav}>
             <h4 className="text-base md:text-lg font-extrabold ">
               get<span className="text-purple5">linked</span>
             </h4>
@@ -35,39 +39,43 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div className={`bg-purple3 flex  ${isNavOpen ? " flex-col w-[300px]  space-y-5 text-base mt-24  md:text-sm my-10 pl-5 absolute right-0" : "hidden md:flex md:items-center md:space-x-10 lg:space-x-20   "}`}>
-          <div className={`w-2/3   ${isNavOpen ? " flex items-end justify-end absolute right-5 " : " hidden"}`}>
-            <button className="" onClick={toggleNav}>
-              <Image src={cancelBtn} width={20} alt="" />
+        <div className={`bg-purple3 flex  ${isNavOpen ? " flex-col w-[300px]  space-y-5 text-base   md:text-sm my-10  absolute right-0" : "hidden md:flex md:items-center md:space-x-10 lg:space-x-20   "}`}>
+          <div className={`w-2/3   ${isNavOpen ? " flex items-end justify-end absolute right-5 mt-[-40px]" : " hidden"}`}>
+            <button className="rounded-full border-2 border-[#903AFF]  w-[11px] relative p-2 h-[11px]" onClick={toggleNav}>
+              <Image src={cancelBtn} fill alt="" />
             </button>
           </div>
           <div className="flex flex-col  item-center space-y-4 font-bold  md:flex-row md:space-x-12  md:space-y-0">
             <Link
               href="#"
               className={`hover:text-purple2  ${isLinkActive("/timeline") ? "md:text-[22px]" : ""}`}
+              onClick={closeNav} // Close the navigation menu when link is clicked
             >
               Timeline
             </Link>
             <Link
               href="#"
               className={`hover:text-purple2 ${isLinkActive("/overview") ? "md:text-[22px]" : ""}`}
+              onClick={closeNav} // Close the navigation menu when link is clicked
             >
               Overview
             </Link>
             <Link
               href="#"
               className={`hover:text-purple2 ${isLinkActive("/faqs") ? "md:text-[22px]" : ""}`}
+              onClick={closeNav} // Close the navigation menu when link is clicked
             >
               FAQs
             </Link>
             <Link
               href="contact-us"
               className={`hover:text-purple2 ${isLinkActive("/contact-us") ? "md:text-[22px]" : ""}`}
+              onClick={closeNav} // Close the navigation menu when link is clicked
             >
               Contact
             </Link>
           </div>
-          <div className={`flex  items-center  my-4`}>
+          <div className={`flex  items-center  my-4`} onClick={closeNav}>
             <Link href="/auth">
               <Button btnValue="Register" />
             </Link>
