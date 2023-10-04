0f7598d7161d336b56c096637532b4edf6ba33f5
ajalapraise <akintomiisin@gmail.com>
2023-10-04 10:33:10 +0100

    update

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
-// import { signIn } from "next-auth/react";
 import Link from "next/link";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import Image from "next/image";
+import insta from "../../public/insta.svg";
+import twitter from "../../public/twitter.svg";
+import facebook from "../../public/facebook.svg";
+import linkedin from "../../public/linkedin.svg";
+import { IoIosArrowBack } from 'react-icons/io'
 // import { useSession } from "next-auth/react";
 // import router from "next/router";
 interface FormState {
@@ -54,7 +58,7 @@ const contact: React.FC<FormState> = () => {
 
     const onSubmit = async (event: { preventDefault: () => void }) => {
         event.preventDefault();
-        const formattedPhoneNumber = formData.phoneNumber.startsWith("+61") ? formData.phoneNumber : "+61" + formData.phoneNumber;
+
         // console.log("formdata", formData);
         // const result = await signIn("credentials", {
         //     redirect: false,
@@ -73,11 +77,13 @@ const contact: React.FC<FormState> = () => {
         // }
     };
     return (
-        <div>
-            <div>
-
-            </div>
-            <div>
+        <div className={`  justify-evenly relative items-center px-10 md:px-20 pt-6 md:pt-3 border-b-[1px] min-h-screen border-[#FFFFFF2E] w-full`}>
+            <div className={`flex flex-col`}>
+                <div className={`bg-gradient-to-r from-[#903AFF] to-[#FE34B9] rounded-full w-[30px] h-[30px] flex justify-center items-center`}>
+                    <button className="bg-purple3 flex justify-center items-center w-[28px] h-[27px] rounded-full">
+                        <IoIosArrowBack className={`text-[white] w-[20px] h-[20px]`} />
+                    </button>
+                </div>
                 <div>
                     <h1>
                         Questions or need assistance? Let us know about it
@@ -86,15 +92,11 @@ const contact: React.FC<FormState> = () => {
                         Email us below to any question related to our event
                     </h2>
                 </div>
-                <div className="md:w-[450px]">
+                <div className="w-full p-4 md:w-[450px]">
+
                     <form action="" onSubmit={onSubmit} className={`space-y-5  p-5`}>
-                        <div className={`flex flex-col `}>
-                            <label
-                                htmlFor="firstName"
-                                className={`font-bold text-[16px] px-2 my-1`}
-                            >
-                                fisrtName
-                            </label>
+                        <div className={``}>
+
                             <input
                                 type="firstName"
                                 placeholder="Enter your email-address"
@@ -107,26 +109,18 @@ const contact: React.FC<FormState> = () => {
                             />
                         </div>
                         <div className={`flex flex-col`}>
-                            <label htmlFor="phoneNumber" className={`font-bold text-[16px] my-3`}>
-                                Phone Number <span className={`text-red10`}>*</span>
-                            </label>
-                            <div className={`flex items-center justify-around`}>
-                                <h4 className={`border-medium border-[1px] text-base text-black font-bold p-3 rounded-xl`}>AU +61</h4>
-                                <input type="tel" placeholder='Enter phone number' name='phoneNumber' id='phoneNumber' className={`border-medium border-[1px] text-base text-black font-bold py-3 px-10 tracking-[0.3rem] rounded-xl w-5/6`} value={formData.phoneNumber} onChange={handleChange} onKeyDown={handlePhoneNumberKeyDown} required maxLength={9} minLength={9}
+
+                            <div className={``}>
+                                <input type="tel" placeholder='Enter phone number' name='phoneNumber' id='phoneNumber' className={``} value={formData.phoneNumber} onChange={handleChange} onKeyDown={handlePhoneNumberKeyDown} required maxLength={11} minLength={11}
                                 />
                             </div>
                         </div>
 
                         <div className={`flex flex-col `}>
-                            <label
-                                htmlFor="email"
-                                className={`font-bold text-[16px] px-2 my-1`}
-                            >
-                                Email <span className={`text-red10`}>*</span>
-                            </label>
+
                             <input
                                 type="email"
-                                placeholder="Enter your email-address"
+                                placeholder="Email"
                                 id="email"
                                 name="email"
                                 className={``}
@@ -137,20 +131,59 @@ const contact: React.FC<FormState> = () => {
                         </div>
 
 
+                        <div className={`flex flex-col `}>
 
+                            <textarea
+                                placeholder="Body"
+                                id="body"
+                                name="body"
+                                className={``}
+                                value={formData.message}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
 
-
-                        <div className={`flex justify-center items-center`}>
+                        <div className={`bg-gradient-to-r from-[#903AFF] to-[#FE34B9] rounded-[5px] cursor-pointer flex items-center justify-center `}>
                             <button
                                 type="submit"
-                                className={`w-full bg-purpleBase text-white py-2 px-4 rounded-md hover:bg-purple5 text-sm ${isAllFieldsFilled() ? '' : 'cursor-not-allowed opacity-50'}`}
+                                className={` text-[12px] w-[100px] h-[30px] md:text-[16px]  cursor-pointer text-[white] md:w-[120px] md:h-[45px] font-bold hover:bg-purple1 rounded-[5px]  py-1 px-2 text-center justify-center items-center ${isAllFieldsFilled() ? '' : 'cursor-not-allowed opacity-50'}`}
                                 disabled={!isAllFieldsFilled()}
                             >
-                                Log In
+                                Submit
                             </button>
                         </div>
                     </form>
                 </div>
+                <div>
+                    <div className={`hidden md:flex`}>
+                        <h1>Get in touch</h1>
+                        <h3>Contact Information</h3>
+                        <h3>27,Alara Street, Yaba 100012, Lagos State</h3>
+                        <h3>Call Us : 07067981819</h3>
+                        <h3>we are open from Monday-Friday, 08:00am - 05:00pm</h3>
+                    </div>
+
+                    <div>
+                        <h1>Share on</h1>
+                        <div className=" flex space-x-2 md:space-x-4 items-center">
+                            <Link href='https://www.instagram.com/accounts/login/' className={` items-center flex justify-center relative w-[20px] h-[20px]`} target="_blank">
+                                <Image src={insta} fill alt="" />
+                            </Link>
+                            <Link href='https://twitter.com/login/' className={` items-center flex justify-center relative w-[20px] h-[20px]`} target="_blank">
+                                <Image src={twitter} fill alt="" />
+                            </Link>
+                            <Link href='https://www.facebook.com/login/' className={` items-center flex justify-center relative w-[20px] h-[20px]`} target="_blank">
+                                <Image src={facebook} fill alt="" />
+                            </Link>
+                            <Link href='https://www.linkedin.com/login/' className={` items-center flex justify-center relative w-[20px] h-[20px]`} target="_blank">
+                                <Image src={linkedin} fill alt="" />
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     )
