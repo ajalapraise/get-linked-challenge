6ffa9d7ba231fef49169129b290d3649b9730692
ajalapraise <akintomiisin@gmail.com>
2023-10-05 12:22:25 +0100

    update

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -80,27 +80,28 @@ const contact: React.FC<FormState> = () => {
     };
     return (
         <div className={`  justify-evenly relative items-center px-10 md:px-20 py-10 md:py-24 border-b-[1px] min-h-screen border-[#FFFFFF2E] w-full`}>
-            <div className="w-full md:p-5  h-[670px] justify-center items-center ">
-                <div className={`md:p-5 flex flex-col md:justify-between md:items-center  md:flex-row-reverse  md:w-full `}>
-                    <div className={`bg-gradient-to-r from-[#903AFF] to-[#FE34B9] rounded-full w-[30px] h-[30px] flex justify-center items-center  mb-12 md:hidden`}>
+            <div className="w-full md:p-5  md:h-[670px] justify-center items-center ">
+                <div className={`md:p-5 p-2 flex flex-col md:justify-between md:items-center  md:flex-row-reverse  md:w-full `}>
+                    <div className={`bg-gradient-to-r from-[#903AFF] to-[#FE34B9] rounded-full w-[30px] h-[30px] flex justify-center items-center   mb-12 md:hidden`}>
                         <Link href='/'>
                             <button className="bg-purple3 flex justify-center items-center w-[28px] h-[27px] rounded-full">
                                 <IoIosArrowBack className={`text-[white] w-[15px] h-[15px]`} />
                             </button>
                         </Link>
                     </div>
-                    <div className={`flex md:border-[1px]  md:border-[blue]  md:w-[650px] md:justify-center md:items-center md:px-5 md:py-14 shadow-md`}>
-                        <div className={`flex flex-col space-y-7  md:justify-center md:items-start `}>
-                            <h1 className={`${revalia.className} font-extrabold text-[14px] w-[230px]  text-[#D434FE]`}>
-                                Questions or need assistance? <br />Let us know about it
-                            </h1>
-                            <h2 className={`font-bold text-[12px] w-[240px] md:hidden`}>
-                                Email us below to any question related to our event
-                            </h2>
-                            <div className="w-full my-10   md:w-[450px]">
-                                <form action="" onSubmit={onSubmit} className={`space-y-8 `}>
+                    <div className={`m:flex md:border-[1px] w-full md:border-[blue]  md:w-[650px] md:justify-center md:items-center  md:px-5 md:py-14 shadow-md`}>
+                        <div className={`md:flex md:flex-col space-y-7 mx-auto  md:justify-center md:items-center md:w-[550px] `}>
+                            <div className={` space-y-7  `}>
+                                <h1 className={`${revalia.className} font-extrabold text-[14px] w-[230px]  text-[#D434FE]`}>
+                                    Questions or need assistance? <br />Let us know about it
+                                </h1>
+                                <h2 className={`font-bold text-[12px] w-[240px] md:hidden`}>
+                                    Email us below to any question related to our event
+                                </h2>
+                            </div>
+                            <div className="w-full my-10 justify-center items-center  md:w-[450px]">
+                                <form action="" onSubmit={onSubmit} className={`space-y-8   w-full `}>
                                     <div className={`border-[1px] rounded-md border-[white]`}>
-
                                         <input
                                             type="firstName"
                                             placeholder="Enter your Name"
@@ -113,7 +114,6 @@ const contact: React.FC<FormState> = () => {
                                         />
                                     </div>
                                     <div className={`border-[1px] rounded-md border-[white]`}>
-
                                         <div className={``}>
                                             <input type="tel" placeholder='Enter phone number' name='phoneNumber' id='phoneNumber'
                                                 className={`bg-transparent h-[40px] px-3 text-[white] w-full font-bold text-[16px] focus:outline-none`}
@@ -125,7 +125,6 @@ const contact: React.FC<FormState> = () => {
                                     </div>
 
                                     <div className={`border-[1px] rounded-md border-[white]`}>
-
                                         <input
                                             type="email"
                                             placeholder="Email"
@@ -169,7 +168,7 @@ const contact: React.FC<FormState> = () => {
                         <div className={`hidden md:flex md:flex-col space-y-7  text-[white]`}>
                             <h1 className={`${revalia.className} text-lg text-[#D434FE] font-extrabold`}>Get in touch</h1>
                             <h3 className="text-base w-[100px] font-extrabold">Contact Information</h3>
-                            <h3 className="text-base w-[100px] font-extrabold">27,Alara Street, Yaba 100012, Lagos State</h3>
+                            <h3 className="text-base w-[110px] font-extrabold">27, Alara Street, Yaba 100012, Lagos State</h3>
                             <h3 className="text-base w-[200px] font-extrabold">Call Us : 07067981819</h3>
                             <h3 className="text-base w-[250px] font-extrabold">we are open from Monday-Friday, 08:00am - 05:00pm</h3>
                         </div>
