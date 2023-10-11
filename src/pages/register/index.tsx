61b0bb592c0ef8190c13d3f34024d4724a55f60a
ajalapraise <akintomiisin@gmail.com>
2023-10-11 21:19:43 +0100

    update

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -6,7 +6,9 @@ import Image from "next/image";
 import manWalking from "../../../public/man-walking.svg";
 import ladyWalking from "../../../public/lady-walking.svg";
 import register from "../../../public/register.svg";
-import { data } from "autoprefixer";
+import success from '../../../public/congratulations.svg'
+import winkEmoji from '../../../public/wink-emoji.svg'
+import { Button } from "@/components/Button/Button";
 
 interface FormState {
   teamName: string;
@@ -18,7 +20,7 @@ interface FormState {
   groupSize: number;
 }
 
-const index = () => {
+const index: React.FC<FormState> = () => {
   const [data, setData] = useState([]);
 
 
@@ -123,139 +125,175 @@ const index = () => {
   }
 
   return (
-    <div className={`border-b-[1px] p-10 space-y-5 border-[#FFFFFF2E] md:space-y-10 md:px-20 md:py-14 flex flex-col md:flex-row justify-between items-center`}>
-      <div className=" w-full md:w-2/3 flex flex-col md:flex-row justify-start">
-        <h1 className="text-[#D434FE] md:hidden mb-5 flex text-lg  font-extrabold">Register</h1>
+    <div className="flex justify-center items-center flex-col relative">
+      <div className={`border-b-[1px] p-10 space-y-5 border-[#FFFFFF2E] md:space-y-10 md:px-20 md:py-14 flex flex-col md:flex-row justify-between items-center`}>
+        <div className=" w-full md:w-2/3 flex flex-col md:flex-row justify-start">
+          <h1 className="text-[#D434FE] md:hidden mb-5 flex text-lg  font-extrabold">Register</h1>
 
-        <div className=" w-full h-[250px]  sm:w-[300px] sm:h-[350px] relative md:w-[500px] md:h-[600px]">
-          <Image src={register} fill alt="" />
+          <div className=" w-full h-[250px]  sm:w-[300px] sm:h-[350px] relative md:w-[500px] md:h-[600px]">
+            <Image src={register} fill alt="" />
+          </div>
+        </div>
+        <div className={`w-full  my-10  md:rounded-md md:bg-[#FFFFFF08] md:p-10 md:px-16 md:w-[700px] md:flex md:flex-col md:shadow-lg`}>
+          <div className="flex flex-col space-y-5 md:w-[500px] justify-center ">
+            <h1 className="text-[#D434FE] hidden md:flex md:text-lg mb-5 font-extrabold">Register</h1>
+            <div className="flex space-x-2 md:space-x-5  items-center">
+              <h2 className="font-bold text-base md:text-[18px] ">Be part of this movement! </h2>
+              <div className="flex justify-center w-[70px] md:w-[100px] mt-[-13px] space-x-1 items-center border-b-2 border-[#D434FE] border-dashed">
+                <div className={`relative w-[30px] h-[30px]`}>
+                  <Image src={ladyWalking} fill alt="" />
+                </div>
+                <div className={`relative  w-[30px] h-[30px]`}>
+                  <Image src={manWalking} fill alt="" />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h1 className="font-bold text-[20px] md:text-[27px] tracking-wider mb-4">CREATE YOUR ACCOUNT</h1>
+            </div>
+          </div>
+          <div className='w-full'>
+            <form action="" onSubmit={onSubmit}>
+              <div className={`space-y-4 mb-10`}>
+                <div className={`flex flex-col md:flex-row justify-between space-y-4`}>
+                  <div className={`flex flex-col md:basis-[47%]`}>
+                    <label htmlFor="teamName" className={`font-bold text-base md:text-[16px]  my-3`}>
+                      Team's Name
+                    </label>
+                    <input type="text" placeholder='Enter the name of your group' id='teamName' name='teamName' className={` border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.teamName} onChange={handleChange} required
+
+                    />
+                  </div>
+
+                  <div className={`flex flex-col md:basis-[47%]`}>
+                    <label htmlFor="phoneNumber" className={`font-bold text-base md:text-[16px] my-3`}>
+                      Phone
+                    </label>
+                    <input type="tel" placeholder='Enter your phone number' name='phoneNumber' id='phoneNumber' className={`  border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm `} value={formData.phoneNumber} onChange={handleChange} onKeyDown={handlePhoneNumberKeyDown} required maxLength={11} minLength={11}
+                    />
+                  </div>
+                </div>
+
+                <div className={`flex flex-col md:flex-row justify-between space-y-4`}>
+                  <div className={`flex flex-col md:basis-[47%]`}>
+                    <label htmlFor="email" className={`font-bold text-base md:text-[16px] my-3`}>
+                      Email
+                    </label>
+                    <input type="email" placeholder='Enter your email-address' id='email' name='email' className={`  border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.email} onChange={handleChange} required
+                    />
+                  </div>
+
+                  <div className={`flex flex-col md:basis-[47%]`}>
+                    <label htmlFor="projectTopic" className={`font-bold text-base md:text-[16px]  my-3`}>
+                      Project Topic
+                    </label>
+                    <input type="text" placeholder='What is your group project topic' id='projectTopic' name='projectTopic' className={` border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.projectTopic} onChange={handleChange} required
+                    />
+                  </div>
+                </div>
+
+                <div className={`flex justify-between space-x-5`}>
+                  <div className={`flex flex-col basis-[50%] md:basis-[47%]`}>
+                    <label htmlFor="category" className={`font-bold text-base md:text-[16px]  my-3`}>
+                      Category
+                    </label>
+                    <select name="category" id="category" className={`border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.category} onChange={handleChange} required >
+                      <option value="" disabled hidden>Select your Category</option>
+                      {data.map((name, index) => (
+                        <option key={index} className="bg-purple3 text-[white]">{name}</option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className={`flex flex-col basis-[30%] md:basis-[47%]`}>
+                    <label htmlFor="groupSize" className={`font-bold text-base md:text-[16px] my-3`}>
+                      Group Size <span className={`text-red10`}></span>
+                    </label>
+
+                    <select name="groupSize" id="groupSize" className={`border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.groupSize} onChange={handleChange} required >
+                      <option value="" disabled hidden>Select</option>
+                      <option className="bg-purple3 text-[white]">1</option>
+                      <option className="bg-purple3 text-[white]">2</option>
+                      <option className="bg-purple3 text-[white]">3</option>
+                      <option className="bg-purple3 text-[white]">4</option>
+                      <option className="bg-purple3 text-[white]">5</option>
+                      <option className="bg-purple3 text-[white]">6</option>
+                      <option className="bg-purple3 text-[white]">7</option>
+                      <option className="bg-purple3 text-[white]">8</option>
+                      <option className="bg-purple3 text-[white]">9</option>
+                      <option className="bg-purple3 text-[white]">10</option>
+                      <option className="bg-purple3 text-[white]">11</option>
+                      <option className="bg-purple3 text-[white]">12</option>
+                      <option className="bg-purple3 text-[white]">13</option>
+                      <option className="bg-purple3 text-[white]">14</option>
+                      <option className="bg-purple3 text-[white]">15</option>
+
+
+                    </select>
+                  </div>
+                </div>
+
+                <div>
+                  <h1 className="text-[#FF26B9] text-[12px] font-extrabold md:text-[16px]">Please review your registration details before submitting</h1>
+                </div>
+
+
+                <div className={`space-x-2`}>
+                  <input onChange={handleChange} type="checkbox" name="agreement" id="agreement" required />
+                  <label htmlFor="agreement" className={`font-bold text-base`}>I agreed with the event terms and conditions  and privacy policy</label>
+                </div>
+
+              </div>
+
+              <div className={`flex justify-center items-center`}>
+                <div className={` rounded-[5px] flex items-center w-2/3 md:w-full justify-center `}>
+                  <button
+                    type="submit"
+                    className={` text-[16px]   text-[white] font-extrabold  rounded-[5px] py-1 px-2 text-center justify-center w-full  h-[50px] bg-gradient-to-r from-[#903AFF] to-[#FE34B9] items-center cursor-not-allowed ${isAllFieldsFilled() ? 'hover:bg-purple1 cursor-pointer' : 'opacity-50'}`}
+                    disabled={!isAllFieldsFilled()}>
+                    Register Now
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <div className={`w-full  my-10  md:rounded-md md:bg-[#FFFFFF08] md:p-10 md:px-16 md:w-[700px] md:flex md:flex-col md:shadow-lg`}>
-        <div className="flex flex-col space-y-5 md:w-[500px] justify-center ">
-          <h1 className="text-[#D434FE] hidden md:flex md:text-lg mb-5 font-extrabold">Register</h1>
-          <div className="flex space-x-2 md:space-x-5  items-center">
-            <h2 className="font-bold text-base md:text-[18px] ">Be part of this movement! </h2>
-            <div className="flex justify-center w-[70px] md:w-[100px] mt-[-13px] space-x-1 items-center border-b-2 border-[#D434FE] border-dashed">
-              <div className={`relative w-[30px] h-[30px]`}>
-                <Image src={ladyWalking} fill alt="" />
-              </div>
-              <div className={`relative  w-[30px] h-[30px]`}>
-                <Image src={manWalking} fill alt="" />
+      <div className="p-2 flex flex-col w-3/4 h-[500px] md:w-[550px] justify-center border-[1px] border-[#D434FE] absolute space-y-10">
+        <div className="relative w-[250px] flex justify-center items-center h-[100px]">
+          <Image src={success} fill alt="" />
+        </div>
+        <div className="flex flex-col space-y-10">
+          <div className="text-center text-sm font-extrabold">
+            <h1>Congratulations  </h1>
+            <h1>You have successfully Registered!</h1>
+          </div>
+
+          <div className="flex flex-col text-center text-[16px] font-extrabold">
+            <h3>Yes, it was easy and you did it! </h3>
+            <div className="flex">
+              <h3>check your mail box for next step</h3>
+              <div className="relative w-[20px] h-[20px]">
+                <Image src={winkEmoji} fill alt="" />
               </div>
             </div>
           </div>
+        </div>
 
-          <div>
-            <h1 className="font-bold text-[20px] md:text-[27px] tracking-wider mb-4">CREATE YOUR ACCOUNT</h1>
+
+        <div className={`flex justify-center items-center`}>
+          <div className={` rounded-[5px] flex items-center w-2/3 md:w-full justify-center `}>
+            <button
+              type="submit"
+              className={` text-[16px]   text-[white] font-extrabold  rounded-[5px] py-1 px-2 text-center justify-center w-full  h-[50px] bg-gradient-to-r from-[#903AFF] to-[#FE34B9] items-center cursor-pointer hover:bg-purple1`}>
+              Back
+            </button>
           </div>
         </div>
-        <div className='w-full'>
-          <form action="" onSubmit={onSubmit}>
-            <div className={`space-y-4 mb-10`}>
-              <div className={`flex flex-col md:flex-row justify-between`}>
-                <div className={`flex flex-col md:basis-[47%]`}>
-                  <label htmlFor="teamName" className={`font-bold text-[16px]  my-3`}>
-                    Team's Name
-                  </label>
-                  <input type="text" placeholder='Enter the name of your group' id='teamName' name='teamName' className={` border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.teamName} onChange={handleChange} required
 
-                  />
-                </div>
-
-                <div className={`flex flex-col md:basis-[47%]`}>
-                  <label htmlFor="phoneNumber" className={`font-bold text-[16px] my-3`}>
-                    Phone
-                  </label>
-                  <input type="tel" placeholder='Enter your phone number' name='phoneNumber' id='phoneNumber' className={`  border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm `} value={formData.phoneNumber} onChange={handleChange} onKeyDown={handlePhoneNumberKeyDown} required maxLength={11} minLength={11}
-                  />
-                </div>
-              </div>
-
-              <div className={`flex flex-col md:flex-row justify-between`}>
-                <div className={`flex flex-col md:basis-[47%]`}>
-                  <label htmlFor="email" className={`font-bold text-[16px] my-3`}>
-                    Email
-                  </label>
-                  <input type="email" placeholder='Enter your email-address' id='email' name='email' className={`  border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.email} onChange={handleChange} required
-                  />
-                </div>
-
-                <div className={`flex flex-col md:basis-[47%]`}>
-                  <label htmlFor="projectTopic" className={`font-bold text-[16px]  my-3`}>
-                    Project Topic
-                  </label>
-                  <input type="text" placeholder='What is your group project topic' id='projectTopic' name='projectTopic' className={` border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.projectTopic} onChange={handleChange} required
-                  />
-                </div>
-              </div>
-
-              <div className={`flex justify-between space-x-5`}>
-                <div className={`flex flex-col basis-[50%] md:basis-[47%]`}>
-                  <label htmlFor="category" className={`font-bold text-[16px]  my-3`}>
-                    Category
-                  </label>
-                  <select name="category" id="category" className={`border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.category} onChange={handleChange} required >
-                    <option value="" disabled hidden>Select your Category</option>
-                    {data.map((name, index) => (
-                      <option key={index} className="bg-purple3 text-[white]">{name}</option>
-                    ))}
-                  </select>
-                </div>
-
-                <div className={`flex flex-col basis-[30%] md:basis-[47%]`}>
-                  <label htmlFor="groupSize" className={`font-bold text-[16px]  my-3`}>
-                    Group Size <span className={`text-red10`}></span>
-                  </label>
-
-                  <select name="groupSize" id="groupSize" className={`border-[1px] border-[white] bg-transparent text-base text-[white] font-bold p-3 rounded-sm`} value={formData.groupSize} onChange={handleChange} required >
-                    <option value="" disabled hidden>Select</option>
-                    <option className="bg-purple3 text-[white]">1</option>
-                    <option className="bg-purple3 text-[white]">2</option>
-                    <option className="bg-purple3 text-[white]">3</option>
-                    <option className="bg-purple3 text-[white]">4</option>
-                    <option className="bg-purple3 text-[white]">5</option>
-                    <option className="bg-purple3 text-[white]">6</option>
-                    <option className="bg-purple3 text-[white]">7</option>
-                    <option className="bg-purple3 text-[white]">8</option>
-                    <option className="bg-purple3 text-[white]">9</option>
-                    <option className="bg-purple3 text-[white]">10</option>
-                    <option className="bg-purple3 text-[white]">11</option>
-                    <option className="bg-purple3 text-[white]">12</option>
-                    <option className="bg-purple3 text-[white]">13</option>
-                    <option className="bg-purple3 text-[white]">14</option>
-                    <option className="bg-purple3 text-[white]">15</option>
-
-
-                  </select>
-                </div>
-              </div>
-
-              <div>
-                <h1 className="text-[#FF26B9] text-[12px] font-extrabold md:text-[16px]">Please review your registration details before submitting</h1>
-              </div>
-
-
-              <div className={`space-x-2`}>
-                <input onChange={handleChange} type="checkbox" name="agreement" id="agreement" required />
-                <label htmlFor="agreement" className={`font-bold text-base`}>I agreed with the event terms and conditions  and privacy policy</label>
-              </div>
-
-            </div>
-
-            <div className={`flex justify-center items-center`}>
-              <div className={` rounded-[5px] flex items-center w-2/3 md:w-full justify-center `}>
-                <button
-                  type="submit"
-                  className={` text-[16px]   text-[white] font-extrabold  rounded-[5px] py-1 px-2 text-center justify-center w-full  h-[50px] bg-gradient-to-r from-[#903AFF] to-[#FE34B9] items-center cursor-not-allowed ${isAllFieldsFilled() ? 'hover:bg-purple1 cursor-pointer' : 'opacity-50'}`}
-                  disabled={!isAllFieldsFilled()}>
-                  Register Now
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
       </div>
+
     </div>
   );
 };
